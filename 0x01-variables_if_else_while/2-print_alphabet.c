7e29ceb648079129abca4bf819ae5fefc830479f
Anneline-Nombeko <a.nombeko@outlook.com>
2023-04-24 16:04:33 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-entry position
+ * description:'print upper case letter'
+ * Return :always 0
+ */
+int main(void)
+{
+int n = 97;
+while ( n<= 22)
+{
+	putchar(n);
+	n++;
+}
+putchar(n);
+return (0);
+}
