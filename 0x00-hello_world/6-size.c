a90cdcb6fb9fac1e1d7e4c02cf83bb04e4061360
Anneline-Nombeko <a.nombeko@outlook.com>
2023-04-23 22:09:05 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("char size =1byte");
+	printf("unassigned char=1byte");
+	printf("signed char=1byte");
+	printf("intteger size=1 or 4 byte");
+	printf("unsigned int=2 or 4 bytes");
+        printf("short=2 bytes\t\t\t\t\t\t");
+	printf("unsigned short=2 bytes\t");
+	printf("long=8 bytes or 4bytes for 32 bit OS");
+	printf("unsigned long");
+}
