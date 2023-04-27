09b8c7ec3506a6b9679f3ee0fb507be4b76ef65f
Anneline-Nombeko <a.nombeko@outlook.com>
2023-04-27 08:14:31 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 /**
 *main:entry point
 *description:'prints 10x alphabets'
 *Return:always zero
 */
 
+int  main(void)
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}
