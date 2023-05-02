b07c8ce0b8b033ffa568bd7381f43742b7836eab
Anneline-Nombeko <a.nombeko@outlook.com>
2023-05-02 07:15:12 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main:Put - prints a string,followed by a new line
+ * @str: print string
+ * return:success
+ */
+int _strlen(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
