2dca42a9f3877d24e82fab94bbd8a7407b00f060
Anneline-Nombeko <a.nombeko@outlook.com>
2023-05-02 06:23:00 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_strlen - return the length of a string
+*@s:string
+*return:length
+*/
+int _strlen(char *s)
+{
+	int longi = 0;
+	while (*s != '\0')
+	{
+		longi++;
+	}
+	return(longi);
+}
