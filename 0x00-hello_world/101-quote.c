377085c773f6537f2a43af5e2c313238c2aaf035
Anneline-Nombeko <a.nombeko@outlook.com>
2023-05-06 02:13:07 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
  * main:write c program that print string
  * not allowed to use printf or puts
  *Return:one
- */
+*/
 
 int main(void)
 {
-	write(1,"and that piece of art is useful\" \- Dora Korpar, 2015-10-19",strlen("and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n");
-			}			}
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
 
